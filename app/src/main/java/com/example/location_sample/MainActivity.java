0145eb3d358c359151ebd01c5795cdbf03dc9d5e
RinaKawase <rinakawase@srC02Z434WLVCH.local>
2024-06-17 18:13:33 +0900

    [add]create HomeFragment layout #14

--- app/src/main/java/com/example/location_sample/MainActivity.java
+++ app/src/main/java/com/example/location_sample/MainActivity.java
@@ -2,19 +2,22 @@ package com.example.location_sample;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // ０、レイアウトファイルをアクティビティの画面に表示
         setContentView(R.layout.activity_main);
+        // １、HomeFragmentを追加するトランザクションを開始（フラグメントをActivityに追加する）
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, new HomeFragment())
+                .commit();
+        // ２、システムバーに重ならないようにする
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
